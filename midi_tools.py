4f27815d86305dc2da67178183af5e32f098156a
Huub van Thienen <huub@timeless-way.nl>
2018-05-06 23:43:32 +0200

    Channels are now set per musicule rather than globally in the midi_tape.

--- midi_tools.py
+++ midi_tools.py
@@ -1,4 +1,3 @@
-import midi_constants as mc
 import midiutil as util
 import sound
 
@@ -9,22 +8,29 @@ class Musicule:
 	
 class Note(Musicule):
 	
-	def __init__(self, pitch=mc.c[3], volume=90, duration=1, legato=1.0):
+	def __init__(self, pitch=59, volume=90, duration=1, legato=1.0, channel=None):
 		super().__init__(duration)
 		self.pitch = pitch
 		self.volume = volume
 		self.legato = legato
+		self.channel = channel
+	
+	def set_channel(self, channel):
+		self.channel = channel
 	
 	def play(self, tape):
 		start_time = tape.time
 		legato_duration = self.legato * self.duration
-		tape.add_note(self.pitch, start_time, legato_duration, self.volume)
-		tape.time = start_time + self.duration
+		tape.add_note(self.channel, self.pitch, start_time, legato_duration, self.volume)
+		tape.time += self.duration
 
 class Rest(Musicule):
 	
 	def __init__(self, duration=1):
 		super().__init__(duration)
+	
+	def set_channel(self, channel):
+		pass
 		
 	def play(self, tape):
 		tape.time += self.duration
@@ -38,6 +44,10 @@ class Seq(Musicule):
 		for m in musicules:
 			duration += m.duration
 		self.duration = duration
+	
+	def set_channel(self, channel):
+		for m in self.musicules:
+			m.set_channel(channel)
 	
 	def play(self, tape):
 		for m in self.musicules:
@@ -54,6 +64,10 @@ class Par(Musicule):
 				duration = m.duration
 		self.duration = duration
 	
+	def set_channel(self, channel):
+		for m in self.musicules:
+			m.set_channel(channel)
+	
 	def play(self, tape):
 		time = tape.time
 		for m in self.musicules:
@@ -63,12 +77,16 @@ class Par(Musicule):
 
 class Instrument(Musicule):
 	
-	def __init__(self, instrument):
+	def __init__(self, channel, instrument):
 		super().__init__()
 		self.instrument = instrument
+		self.channel = channel
+	
+	def set_channel(self, channel):
+		self.channel = channel
 	
 	def play(self, tape):
-		tape.change_instrument(self.instrument)
+		tape.change_instrument(self.channel, self.instrument)
 
 class Tempo(Musicule):
 	
@@ -76,28 +94,58 @@ class Tempo(Musicule):
 		super().__init__()
 		self.tempo = tempo
 	
+	def set_channel(self, channel):
+		pass
+	
 	def play(self, tape):
 		tape.set_tempo(self.tempo)
+
+class Channel(Musicule):
+	
+	def __init__(self, channel):
+		super().__init__()
+		self.channel = channel
+	
+	def set_channel(self, channel):
+		pass
+	
+	def play(self, tape):
+		tape.channel = self.channel
+		
+class Track(Musicule):
+	
+	def __init__(self, track):
+		super().__init__()
+		self.track = track
+	
+	def set_channel(self, channel):
+		pass
+	
+	def play(self, tape):
+		tape.current_track = self.track
 		
 class Midi_tape:
 	
-	def __init__(self, name, channel=0):
-		self.midi_file = util.MIDIFile(1, adjust_origin=False)
+	def __init__(self, name, track=0, channel=0, numTracks=1):
+		self.midi_file = util.MIDIFile(numTracks, adjust_origin=False)
 		self.name = name
+		self.current_track = track
 		self.channel = channel
 		self.time = 0
 	
-	def add_note(self, pitch, time, duration, volume):
-		self.midi_file.addNote(track=0, channel=self.channel, 
+	def add_note(self, channel, pitch, time, duration, volume):
+		chan = channel if channel else self.channel
+		self.midi_file.addNote(track=self.current_track, channel=chan, 
 		pitch=pitch, time=time, duration=duration, volume=volume)
 	
 	def set_tempo(self, tempo=60, time=-1):
 		if time == -1:
 			time = self.time
-		self.midi_file.addTempo(0, time, tempo)
+		self.midi_file.addTempo(self.current_track, time, tempo)
 		
-	def change_instrument(self, instrument):
-		self.midi_file.addProgramChange(0, self.channel, self.time, instrument)
+	def change_instrument(self, channel, instrument):
+		chan = channel if channel else self.channel
+		self.midi_file.addProgramChange(self.current_track, chan, self.time, instrument)
 		
 	def save(self):
 		with open(self.name + '.mid', 'wb') as output_file:
@@ -106,11 +154,14 @@ class Midi_tape:
 	def play(self):
 		self.save()
 		player = sound.MIDIPlayer(self.name + '.mid')
-		print(self.name, player.duration)
 		player.play()
 		while player.current_time < player.duration+1:
 			pass
 		player.stop()
-		print('Einde')
 
-		
+def perform(musicule, tape=None):
+	if not tape:
+		tape = Midi_tape('temp')
+	musicule.play(tape)
+	tape.play()
+	return tape
